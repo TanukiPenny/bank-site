60697a4e58f3817b61aa9527fbeae8f141262f03
Penny <penny@penny.moe>
2024-06-13 10:43:33 -0400

    test

--- src/components/basicInfo.tsx
+++ src/components/basicInfo.tsx
@@ -11,7 +11,7 @@ function BasicInfo() {
                 <Pfp></Pfp>
                 <div className="infoText">Hai! I am {ageFromDateOfBirthday(new Date(2001, 10, 18))} years old and currently attending university for computer science. I made this
                     page as a simple landing for my domain. If you would like to see some of what I have done you can
-                    checkout my Github page <a href="https://github.com/TanukiPenny">here!</a>
+                    checkout my Github page <a href="https://github.com/TanukiPenny">here! test</a>
                 </div>
             </div>
         </>
